c7c81a266e032b88f42e74fd02f755ceada2bb17
RafaelRCamargo <rafaelrakochinski@gmail.com>
2022-07-21 17:36:20 -0300

    fix: ourstores pagination fix

--- node/clients/example.ts
+++ node/clients/example.ts
@@ -2,15 +2,10 @@ import { ExternalClient, InstanceOptions, IOContext } from '@vtex/api'
 
 export class Example extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
-    super(``, context, {
-      ...options,
-      headers: {
-        Authorization: `Bearer `,
-      },
-    })
+    super(``, context, options)
   }
 
   public async test() {
-    return this.http.get(`https://4006c06b-d8de-4361-8e53-6f7f2b431d32.mock.pstmn.io`)
+    return this.http.get(`https://4006c06b-d8de-4361-8e53-6f7f2b431d32.mock.pstmn.io/link`)
   }
 }
